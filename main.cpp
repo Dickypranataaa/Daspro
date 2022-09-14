69178829be73453a335e713ff0a0006f664bb602
Dickypranataaa <111202214615@mhs.dinus.ac.id>
2022-09-14 17:18:21 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Biodata Diri" << endl;
+    cout << "Nama Lengkap : Dicky Pranata" << endl;
+    cout << "Nama Panggilan : Dicky" << endl;
+    cout << "NIM : A11.2022.14615" << endl;
+    cout << "Jenis Kelamin : Laki-Laki" << endl;
+    cout << "Tempat & Tanggal Lahir : Tegal, 9 November 2003" << endl;
+    cout << "Hobi : Futsal" << endl;
+
+    return 0;
+}
